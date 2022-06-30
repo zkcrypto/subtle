1955e495584c0e3285742389ea44c71da4c31fe8
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2022-06-30 00:34:28 -0400

    rustfmt change

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,6 @@
 #![deny(missing_docs)]
 #![doc(html_root_url = "https://docs.rs/subtle-ng/2.5.0")]
 
-
 #[cfg(feature = "std")]
 #[macro_use]
 extern crate std;
@@ -728,7 +727,7 @@ macro_rules! generate_unsigned_integer_greater {
                 Choice::from((bit & 1) as u8)
             }
         }
-    }
+    };
 }
 
 generate_unsigned_integer_greater!(u8, 8);
